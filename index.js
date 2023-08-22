015e5c886132c091df147619953e5610ae28ffd7
Kye-Working <kyefworkin@gmail.com>
2023-08-22 04:01:29 +0100

    Variables, Logs & alerts
    - Added variables, logs & alert window

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 /*To view console (Inspect + click console)*/
+
+/*.log used to print to the console*/
+console.log("The start of JavaScript.");
+
+/*Creates an alert box within window*/
+window.alert("Testing alert box...");
+
+/*Varibles*/
+/*(Declaration : var, let, const) (name) = (input)*/
+let day;
+day = 6;
+console.log("Day: " + day);
+
+let month = 3;
+console.log("Month: " + month);
+
+/*Data types*/
+/*Num : Numbers*/
+let age = 21;
+age = age + 1;
+/*, & + used to add additional items*/
+console.log("(Num) Age :", age);
+
+/*String : Characters*/
+let name = "BRYAN";
+console.log("(String) Name :" + name);
+
+/*Boolean : True/False*/
+let student = false;
+console.log("(Boolean) Student :" + student);
