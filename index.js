7413137ca58ce67fa08c5d71b62b6a356d861fc8
Kye-Working <kyefworkin@gmail.com>
2023-08-23 19:38:56 +0100

    Arithmetic expressions
    - Added Arithmetic expressions

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 /*.log used to print to the console*/
 console.log("The start of JavaScript.");
+/* \n adds a line on the console */
+console.log("\n");
 
 /*Creates an alert box within window*/
 window.alert("Testing alert box...");
 
-/*Varibles*/
+/*Variables*/
 /*(Declaration : var, let, const) (name) = (input)*/
 let day;
 day = 6;
@@ -14,11 +16,13 @@ console.log("Day: " + day);
 
 let month = 3;
 console.log("Month: " + month);
+console.log("\n");
 
-/*Data types*/
+/* Data types */
 /*Num : Numbers*/
 let age = 21;
 age = age + 1;
+
 /*, & + used to add additional items*/
 console.log("(Num) Age :", age);
 
@@ -29,3 +33,35 @@ console.log("(String) Name :" + name);
 /*Boolean : True/False*/
 let student = false;
 console.log("(Boolean) Student :" + student);
+console.log("\n");
+
+/* Arithmetic expressions (JS operates under BIDMAS")*/
+let value1 = 10;
+
+value1 = value1 + 3;
+console.log("Added :", value1);
+value1 = value1 - 3;
+console.log("Subtract :", value1);
+value1 = value1 * 3;
+console.log("Times :", value1);
+value1 = value1 / 3;
+console.log("Divid :", value1);
+console.log("\n");
+
+/* Remainder */
+value1 = value1 % 4;
+console.log("Remainder :", value1);
+console.log("\n");
+
+/* Reassigning varible */
+let value2 = 5;
+console.log("Default value :", value2);
+value2 += 1;
+console.log("Added value :", value2);
+value2 -= 2;
+console.log("Subtracted value :", value2);
+value2 *= 2;
+console.log("Timed value :", value2);
+value2 /= 3;
+console.log("divided value :", value2);
+console.log("\n");
